b67fc753b150833fdc380a21c6fb2e36c6feb4f5
HINTTECH\jngabo <jean77789@hotmail.com>
2017-04-11 10:30:26 +0200

    completed version for the client

--- test.js
+++ test.js
@@ -2,45 +2,31 @@ var smallBox = document.getElementById("smallBox");
 var containerSection = document.getElementById("containerSection");
 var fullSectionWidth = containerSection.clientWidth; 
 var leftSide = containerSection.offsetLeft; 
-var rightSide = containerSection.offsetRight; 
+var smallBoxWidth = smallBox.clientWidth;
 var smallBoxPos = leftSide; 
 var marginEqualizer = fullSectionWidth/8;
-var testRealWidth = Math.floor(fullSectionWidth + marginEqualizer);
-
-console.log(testRealWidth);
-console.log(fullSectionWidth);
+var testRealWidth = Math.floor(fullSectionWidth + marginEqualizer) - (smallBoxWidth/2);
 
 
 window.addEventListener("resize", calculateBrowserMetrics);
 
 function calculateBrowserMetrics(){
 
-	 console.log("the window is being resized");
-
-	 smallBox = document.getElementById("smallBox");
-	 containerSection = document.getElementById("containerSection");
 	 fullSectionWidth = containerSection.clientWidth; 
-	 leftSide = containerSection.offsetLeft; 
-	 rightSide = containerSection.offsetRight; 
+	 leftSide = containerSection.offsetLeft;
+
+	 //fullSectionWidth is divided by eight to get 10% of fullSectionWidth because fullSection is 80%
 	 marginEqualizer = fullSectionWidth/8;
-     testRealWidth = Math.floor(fullSectionWidth + marginEqualizer);
-		
-	 //console.log(fullSectionWidth);
-	 //console.log(boundingParentTest); 
+     testRealWidth = Math.floor(fullSectionWidth + marginEqualizer) - (smallBoxWidth/2);
+
+     //1 added to the leftSide so that it doesnt conflict with reverseBlock function
+     smallBoxPos = leftSide + 1;
 }
-
-
-
-//var boundingParentTest = containerSection.getBoundingClientRect();
-
 
 
 startAnimation();
 
 function startAnimation(){
-
-
-	//var fullSectionWidth = 1140; 
 	
 	var interVal;
 
@@ -52,30 +38,21 @@ function startAnimation(){
 
 	function startReverse(){
 		interval = setInterval(reverseBlock, 1);
-	}	
-
+	}
 
 	function forwardBlock(){
-
 		smallBox.style.left = smallBoxPos+'px';
 		smallBoxPos++;
-
-		//console.log(fullSectionWidth);
 
         if (smallBoxPos === testRealWidth){
            clearInterval(interval);
            startReverse();
-           console.log("dit wordt een keer uitgevoerd");
         }
-
-        //console.log(boundingParentTest);
 	}
-
 
 	function reverseBlock(){
 		smallBox.style.left = smallBoxPos+'px';
 		smallBoxPos--;
-		//console.log(leftSide);
 
         if (smallBoxPos === leftSide){
            clearInterval(interval);
